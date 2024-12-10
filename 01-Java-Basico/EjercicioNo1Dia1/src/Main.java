0f57e7d4a54dff00397f08609319b1312783b222
JJPalomino <josepalomino_@outlook.com>
2024-12-10 12:37:20 -0600

    Ultimas actualizaciones del día 1

---
+++ 01-Java-Basico/EjercicioNo1Dia1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi, bienvenido al sistema");
+        String nombre = "Pedro";
+        System.out.println("Hola " + nombre + ", bienvenido al sistema");
+    }
+}
---
+++ 01-Java-Basico/EjercicioNo2Dia1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String nombre = ("Pedro Picapiedra");
+        int edad = 26;
+        double saldo = 561.6;
+        boolean activo = true;
+        System.out.println("Datos del ciente es: " + nombre + " con una edad de " + +edad +
+                " años"+ " y con un saldo de $ "+ saldo+"\nEstado del cliente: " + activo);
+
+    }
+}
